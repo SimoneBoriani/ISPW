771993a8132591e7b2f424567ba6b141b7402918
SimoneBoriani <s.boriani04@gmail.com>
2025-12-12 14:29:25 +0100

    Primo commit

---
+++ src/main/java/controller/PrincipalPageController.java
@@ -0,0 +1,16 @@
+package controller;
+
+
+import javafx.fxml.FXML;
+
+
+import java.awt.event.ActionEvent;
+
+
+public class PrincipalPageController {
+
+    @FXML
+    public void log(ActionEvent actionEvent) {
+        System.out.println("LOGIN");
+    }
+}
---
+++ src/main/java/start/Launcher.java
@@ -0,0 +1,10 @@
+package start;
+
+import javafx.application.Application;
+import view.Main.PrincipalPageApp;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(PrincipalPageApp.class, args);
+    }
+}
