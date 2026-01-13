9f274c1f070874d016fe92a8f4d4a2004bc54caa
SimoneBoriani <s.boriani04@gmail.com>
2026-01-13 16:27:43 +0100

    aggiornamento

--- src/main/java/bean/AggiungiAutoBean.java
+++ src/main/java/bean/AggiungiAutoBean.java
@@ -1,86 +1,93 @@
 package bean;
 
+import model.macchina.Macchina;
+import java.sql.SQLException;
+
 public class AggiungiAutoBean {
 
-    private int car_year;
-    private String car_name;
-    private int car_price;
-    private String car_type;
-    private String car_alimentation;
-    private String car_brand;
-    private int car_seat;
-    private int car_owners;
-    int car_km;
+    private int carYear;
+    private String carName;
+    private int carPrice;
+    private String carType;
+    private String carAlimentation;
+    private String carBrand;
+    private int carSeat;
+    private int carOwners;
+    private int carKm;
 
-    public void setCar_name(String car_name) {
-        this.car_name = car_name;
+    public void setCarName(String carName) {
+        this.carName = carName;
     }
-    public String getCar_name() {
-        return car_name;
-    }
-
-    public void setCar_brand(String car_brand) {this.car_brand = car_brand;}
-    public String getCar_brand() {
-        return car_brand;
+    public String getCarName() {
+        return carName;
     }
 
-    public void setCar_price(int car_price) {this.car_price = car_price;}
-    public int getCar_price() {return car_price;
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+    public String getCarBrand() {
+        return carBrand;
     }
 
-    public void setCar_alimentation(String car_alimentation) {
-        this.car_alimentation = car_alimentation;
+    public void setCarPrice(int carPrice) {
+        this.carPrice = carPrice;
     }
-    public String getCar_alimentation() {
-        return car_alimentation;
-    }
-
-    public void setCar_owners(int car_owners) {
-        this.car_owners = car_owners;
-    }
-    public int getCar_owners() {
-        return car_owners;
+    public int getCarPrice() {
+        return carPrice;
     }
 
-    public void setCar_km(int car_km) {
-        this.car_km = car_km;
+    public void setCarAlimentation(String carAlimentation) {
+        this.carAlimentation = carAlimentation;
     }
-    public int getCar_km() {
-        return car_km;
-    }
-
-    public void setCar_year(int car_year) {
-        this.car_year = car_year;
-    }
-    public int getCar_year() {
-        return car_year;
+    public String getCarAlimentation() {
+        return carAlimentation;
     }
 
-    public void setCar_type(String car_type) {
-        this.car_type = car_type;
+    public void setCarOwners(int carOwners) {
+        this.carOwners = carOwners;
     }
-    public String getCar_type() {
-        return car_type;
-    }
-
-    public void setCar_seat(int car_seat) {
-        this.car_seat = car_seat;
-    }
-    public int getCar_seat() {
-        return car_seat;
+    public int getCarOwners() {
+        return carOwners;
     }
 
-    public AggiungiAutoBean sendinfo() {
+    public void setCarKm(int carKm) {
+        this.carKm = carKm;
+    }
+    public int getCarKm() {
+        return carKm;
+    }
+
+    public void setCarYear(int carYear) {
+        this.carYear = carYear;
+    }
+    public int getCarYear() {
+        return carYear;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarSeat(int carSeat) {
+        this.carSeat = carSeat;
+    }
+    public int getCarSeat() {
+        return carSeat;
+    }
+
+    public Macchina sendInfo() {
         try {
-            // Chiamiamo il metodo del DAO passando questo oggetto Bean
-            // DaoMacchine.insert si occuperà di aprire la connessione e fare la query
-            model.macchina.dao.DaoMacchine.insert(this);
 
+            Macchina nuovaAuto = model.macchina.dao.DaoMacchine.insert(this);
             System.out.println("Salvataggio completato con successo nel database!");
-        } catch (java.sql.SQLException e) {
+            return nuovaAuto;
+        } catch (SQLException e) {
             System.err.println("Errore durante il salvataggio dell'auto: " + e.getMessage());
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 }
--- src/main/java/controller/CatalogoController.java
+++ src/main/java/controller/CatalogoController.java
@@ -4,13 +4,16 @@ import bean.CatalogoBean;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import model.macchina.Macchina;
 import model.macchina.dao.DaoMacchine;
-
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CatalogoController {
@@ -33,6 +36,78 @@ public class CatalogoController {
     private TableColumn<CatalogoBean, String> alimentazione;
     @FXML
     private TableColumn<CatalogoBean, Integer> prezzo;
+    @FXML
+    private TableColumn<CatalogoBean, String> tipologia;
+    @FXML
+    private ImageView searchIcon;
+
+    @FXML
+    void MenuRicerca(MouseEvent event) {
+
+        ContextMenu popup = new ContextMenu();
+
+        TextField txtmodel = new TextField();
+        txtmodel.setPromptText("Modello");
+
+
+        TextField txtbrand = new TextField();
+        txtbrand.setPromptText("Marca");
+
+
+        TextField txtprice = new TextField();
+        txtprice.setPromptText("Prezzo");
+
+
+        TextField txtkm = new TextField();
+        txtkm.setPromptText("Km");
+
+
+        TextField txtalimentation = new TextField();
+        txtalimentation.setPromptText("Alimentazione");
+
+
+        CustomMenuItem itemModel = new CustomMenuItem(txtmodel);
+        CustomMenuItem itemBrand = new CustomMenuItem(txtbrand);
+        CustomMenuItem itemPrice = new CustomMenuItem(txtprice);
+        CustomMenuItem itemKm = new CustomMenuItem(txtkm);
+        CustomMenuItem itemAlimentation = new CustomMenuItem(txtalimentation);
+
+
+        itemModel.setHideOnClick(false);
+        itemBrand.setHideOnClick(false);
+        itemPrice.setHideOnClick(false);
+        itemKm.setHideOnClick(false);
+        itemAlimentation.setHideOnClick(false);
+
+        MenuItem btnCerca = new MenuItem("Ricerca");
+        btnCerca.setOnAction(e -> {
+
+            CatalogoBean bean = new CatalogoBean();
+
+            String model = txtmodel.getText();
+            int price=Integer.parseInt(txtprice.getText());
+            String brand = txtbrand.getText();
+            int km = Integer.parseInt(txtkm.getText());
+            String alimentation=txtalimentation.getText();
+
+
+            bean.setAlimentazione(alimentation);
+            bean.setKm(km);
+            bean.setPrezzo(price);
+            bean.setMarca(brand);
+            bean.setModello(model);
+
+            bean.sendInfo();
+
+            researchRefresh(researchedCars(bean));
+        });
+
+        popup.getItems().addAll(itemModel, itemBrand, itemKm, itemAlimentation, itemPrice, new SeparatorMenuItem(), btnCerca);
+
+        popup.show(searchIcon, event.getScreenX(), event.getScreenY());
+    }
+
+
 
     @FXML
     public void initialize() {
@@ -45,8 +120,35 @@ public class CatalogoController {
         anno.setCellValueFactory(new PropertyValueFactory<>("Anno"));
         alimentazione.setCellValueFactory(new PropertyValueFactory<>("Alimentazione"));
         prezzo.setCellValueFactory(new PropertyValueFactory<>("Prezzo"));
+        tipologia.setCellValueFactory(new PropertyValueFactory<>("Tipologia"));
 
         ObservableList<Macchina> data = FXCollections.observableArrayList(CatalogoBean.getCars());
         tableView.setItems(data);
+    }
+
+    @FXML
+    public void refreshTable(MouseEvent event) throws IOException {
+
+        ObservableList<Macchina> data = FXCollections.observableArrayList(DaoMacchine.getCars());
+        tableView.setItems(data);
+        tableView.refresh();
+    }
+    public void researchRefresh(List<Macchina> lista){
+        ObservableList<Macchina> data = FXCollections.observableArrayList(lista);
+        tableView.setItems(data);
+        tableView.refresh();
+    }
+
+    public List<Macchina> researchedCars(CatalogoBean catalogoBean){
+
+        List<Macchina> researchedCars = new ArrayList<>();
+        try {
+           researchedCars=DaoMacchine.research(catalogoBean);
+        } catch (SQLException e) {
+            System.err.println("Errore nel recupero delle auto: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return researchedCars;
+
     }
 }
